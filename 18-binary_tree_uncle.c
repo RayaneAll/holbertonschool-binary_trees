2d021f229dd6e03e7103846119ac437c8eb9bf84
RayaneAll <9975@holbertonstudents.com>
2024-12-11 15:55:04 +0100

    Correction for 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,5 +12,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	return (binary_tree_sibling(node->parent));
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+
+	return (node->parent->parent->left);
 }
