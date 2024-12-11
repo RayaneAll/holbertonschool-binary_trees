bcbeba99d3007fc3169e0a2bfcc453cf40733505
RayaneAll <9975@holbertonstudents.com>
2024-12-11 15:26:17 +0100

    Correction for 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
 * binary_tree_height - Measures the height of a binary tree
@@ -9,15 +8,13 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
@@ -31,5 +28,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int left_height = tree->left ? (int)binary_tree_height(tree->left) : 0;
+
+	int right_height = tree->right ? (int)binary_tree_height(tree->right) : 0;
+
+	return (left_height - right_height);
 }
