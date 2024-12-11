1b3bbfc3d876ecbb91d44b18c57460710f58db91
RayaneAll <9975@holbertonstudents.com>
2024-12-11 15:37:45 +0100

    Correction for 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
